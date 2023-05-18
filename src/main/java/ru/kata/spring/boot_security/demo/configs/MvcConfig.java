efe0236d865b1a67792f33e7ed26a2fc13088a7e
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-05-19 00:13:56 +0300

    Add files via upload

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,5 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user").setViewName("user");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // jquery.min.css
+        registry.addResourceHandler("/jquery/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/jquery/3.3.1-1/");
+        // popper.min.js
+        registry.addResourceHandler("/popper/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/popper.js/1.14.1/umd/");
+        // bootstrap.min.js; bootstrap.min.css
+        registry.addResourceHandler("/bootstrap/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/4.1.1/");
+        // my js
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -9,14 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.kata.spring.boot_security.demo.service.UserDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SuccessUserHandler successUserHandler;
-    private UserDetailService userDetailService;
+    private final UserDetailService userDetailService;
     @Autowired
     public WebSecurityConfig(SuccessUserHandler successUserHandler, UserDetailService userDetailService) {
         this.successUserHandler = successUserHandler;
@@ -25,18 +24,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .formLogin()
+                .successHandler(successUserHandler)
+                .loginProcessingUrl("/login")
+                .permitAll();
+
+        http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index", "/api/auth").permitAll()
-                .antMatchers("/admin/**", "/api/users/**").hasRole("ADMIN")
+                .antMatchers("/login").anonymous()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").successHandler(successUserHandler)
-                .permitAll()
-                .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
                 .permitAll();
     }
     @Bean
